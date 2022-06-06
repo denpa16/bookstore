3fd480be6c8e5b66e00c8bf5db4808d81ebd7147
denpa16 <den.pavlov.1998@list.ru>
2022-06-06 15:06:03 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"bookstore/controllers" // new
+	"bookstore/models"
+)
+
+func main() {
+	r := gin.Default()
+
+	models.ConnectDatabase()
+
+	r.GET("/books", controllers.FindBooks)
+	r.POST("/books", controllers.CreateBook)
+	r.GET("/books/:id", controllers.FindBook)
+	r.PATCH("/books/:id", controllers.UpdateBook)
+	r.DELETE("/books/:id", controllers.DeleteBook)
+
+	r.Run()
+}
