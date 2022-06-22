6092798e0118919f0574a32a46aae71c6c618438
denpa16 <den.pavlov.1998@list.ru>
2022-06-22 16:07:16 +0300

    add controllers

---
+++ handlers/handlers.go
@@ -0,0 +1,26 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	u, err := url.Parse(r.URL.String())
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal server error"))
+		return
+	}
+
+	params := u.Query()
+	searchKey := params.Get("q")
+	page := params.Get("page")
+	if page == "" {
+		page = "1"
+	}
+
+	fmt.Println("Search Query is: ", searchKey)
+	fmt.Println("Results page is: ", page)
+}
--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-
 	"bookstore/controllers" // new
+	//"bookstore/handlers"    // new
 	"bookstore/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-
+	r.LoadHTMLGlob("templates/*.html")
+	r.Static("/assets", "./assets")
 	models.ConnectDatabase()
 
 	r.GET("/books", controllers.FindBooks)
@@ -17,6 +20,12 @@ func main() {
 	r.GET("/books/:id", controllers.FindBook)
 	r.PATCH("/books/:id", controllers.UpdateBook)
 	r.DELETE("/books/:id", controllers.DeleteBook)
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"books": "sdfd",
+		})
+	})
 
 	r.Run()
 }
